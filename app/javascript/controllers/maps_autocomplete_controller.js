91ee698ae20999aed15644e765050525a3ca185e
Sabrina MOUEDINE <mouedines@cfdt-services.fr>
2023-08-09 10:42:51 +0200

    ajout commentaires ctlr maps_autocomplete

--- app/javascript/controllers/maps_autocomplete_controller.js
+++ app/javascript/controllers/maps_autocomplete_controller.js
@@ -7,22 +7,21 @@ export default class extends Controller {
     this.initializeAutocomplete(this.addressTarget, { types: ['geocode'] });
     this.initializeAutocomplete(this.countryTarget, { types: ['(regions)'] });
   }
-
+  // Partie autocompletion du champ address
   initializeAutocomplete(target, options) {
     let autocomplete = new google.maps.places.Autocomplete(target, options);
-
+    // On va écouter le champ address
     autocomplete.addListener("place_changed", () => {
-      let place = autocomplete.getPlace();
+      let place = autocomplete.getPlace(); // Récupère l'objet `place` contenant les informations sur l'adresse sélectionnée
       if (!place.geometry) {
         console.error("No geometry found for selected place.");
         return;
       }
 
       // Mettre à jour les champs de ville et de pays
-      this.updateLocationFields(place);
+      this.updateLocationFields(place); // Appelle la fonction `updateLocationFields` avec l'objet `place` en tant qu'argument
     });
 
-    // ... le reste du code
   }
 
   updateLocationFields(place) {
@@ -30,11 +29,14 @@ export default class extends Controller {
     this.addressTarget.value = place.formatted_address || "";
     if (place.address_components) {
       place.address_components.forEach((component) => {
+        // Vérifier si le type d'adresse est "country" (pays)
         if (component.types.includes("country")) {
+          // Affecter le code court du pays (par exemple, "FR" pour la France) au champ de pays
           this.countryTarget.value = component.short_name || "";
         }
-        // type "locality" qui correspond généralement à la ville
+        // Vérifier si le type d'adresse est "locality" (généralement la ville)
         if (component.types.includes("locality")) {
+          // Affecter le nom de la ville au champ de région (notez que "region" est peut-être un terme confus ici)
           this.regionTarget.value = component.short_name || "";
         }
       });
